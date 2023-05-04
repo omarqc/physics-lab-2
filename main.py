056b61be2a4b3cc5abb75feb5681578c634923af
omarqc <>
2023-05-04 18:56:14 +0200

    optimized code

--- main.py
+++ main.py
@@ -11,15 +11,55 @@ from pyqtgraph.Qt import QtGui, QtCore
 import pyqtgraph as pg
 
 # display only last N values
-MAX_VIEW = 500
+MAX_VIEW = 350
 
-dev = usb.core.find(idVendor=0x04d8, idProduct=0xfe78)
-if dev.is_kernel_driver_active(0):
-    dev.detach_kernel_driver(0)
+description = {
+    "gaussmeter": {
+        "name": "MAGSYS HGM09s",
+        "idVendor": 0x04d8,
+        "idProduct": 0xfe78
+    },
+    "ohmmeter": {
+        "name": "Keithley 2000 Multimeter",
+        "idVendor": 0x0403,
+        "idProduct": 0x6001
+    }
+}
+
+DEVICES = {}
+
+for d_type in list(description.keys()):
+    IDV = description[d_type]["idVendor"]
+    IDP = description[d_type]["idProduct"]
+
+    devices = tuple(usb.core.find(idVendor={IDV}, idProduct={IDP}, find_all=True))
+    try: 
+        # will give error if device variable is None (not found)
+        device[0]
+    except:
+        print(f"Device {d_type} not found, exiting")
+        exit()
+
+    # this is for case of >1 gaussmeter
+    i = 0
+    for sub_device in devices:
+        name = d_type + str(i)
+        DEVICES[name] = sub_device
+        try:
+            if DEVICES[name].is_kernel_driver_active(0):
+                DEVICES[name].detach_kernel_driver(0)
+            
+            print(f"Succesfully connected to '{NAMES[i]}' ({i})")
+
+        except:
+            print(f"Error in connecting to '{NAMES[i]}'")
+            exit()
+
+        i += 1
 
 
 def get_magnetic_field(device):
-    device.write(1, ':READ?<LF>')
+    device.write(1, ':READ?')
     data = device.read(0x81, 100, 100)
     return time.time(), float(bytearray(list(data)).replace(b'\x00', b'').decode('utf-8'))
 
@@ -28,7 +68,10 @@ def get_temperature():
     return time.time(), randint(0,100)
 
 
-def get_resistance():
+def get_resistance(device):
+    device.write(2, ':MEASure:FRESistance?<LF>')
+    data = device.read(0x81, 100, 100)
+    print(data)
     return time.time(), randint(0,100)
 
 
@@ -38,7 +81,7 @@ times1, times2, times3 = [], [], []
 magnetic_field, resistance, temperature = [], [], []
 
 
-def reset():
+def reset_data():
     global times1, times2, times3
     global magnetic_field, resistance, temperature
     times1, times2, times3 = [], [], []
@@ -72,11 +115,11 @@ temperature_plot.setLabel("bottom", "Time / s")
 temperature_plot.getAxis("left").setTextPen("black")
 temperature_plot.getAxis("bottom").setTextPen("black")
 
-FLAG = True
+PAUSE_FLAG = True
 
 
 def update1():
-    t1, B = get_magnetic_field(dev)
+    t1, B = get_magnetic_field(DEVICES["gaussmeter0"])
     t1 -= REF_TIME
 
     times1.append(t1)
@@ -85,13 +128,13 @@ def update1():
     curve1.setData(times1, magnetic_field)
 
     X = times1[-MAX_VIEW:]
-    if FLAG: magnetic_field_plot.setRange(xRange=[min(X), max(X)])
+    if PAUSE_FLAG: magnetic_field_plot.setRange(xRange=[min(X), max(X)])
  
     QtGui.QApplication.processEvents() 
 
 
 def update2():
-    t2, R = get_resistance()
+    t2, R = get_resistance(DEVICES["ohmmeter"])
     t2 -= REF_TIME
 
     times2.append(t2)
@@ -100,7 +143,7 @@ def update2():
     curve2.setData(times2, resistance)
 
     X = times2[-MAX_VIEW:]
-    if FLAG: resistance_plot.setRange(xRange=[min(X), max(X)])
+    if PAUSE_FLAG: resistance_plot.setRange(xRange=[min(X), max(X)])
 
     QtGui.QApplication.processEvents()
 
@@ -115,7 +158,7 @@ def update3():
     curve3.setData(times3, temperature)
 
     X = times3[-MAX_VIEW:]
-    if FLAG: temperature_plot.setRange(xRange=[min(X), max(X)])
+    if PAUSE_FLAG: temperature_plot.setRange(xRange=[min(X), max(X)])
 
     QtGui.QApplication.processEvents()
 
@@ -124,20 +167,19 @@ REF_TIME = time.time()
 
 while True:
     if keyboard.is_pressed("space"):
-        FLAG = not FLAG
+        PAUSE_FLAG = not PAUSE_FLAG
 
     if keyboard.is_pressed("r"):
+        # reset time and data arrays
         REF_TIME = time.time()
-        reset()
+        reset_data()
     
     else:
+        # make sure that the 3 gaussmeters have been connected.
         update1()
         update2()
+
+        # still have to add code for thermometer (actually just reading resistance -> temperature)
         update3()
 
 pg.QtGui.QApplication.exec_()
-
-# b_file = open("magnetic_field.txt", "a")
-
-# for i in range(len(_times1)):
-#     b_file.write(f"{_times1[i]},{_B[i]},{_R[i], _T[i]}")
