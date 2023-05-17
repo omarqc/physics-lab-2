bb2b399aaac09bb54868e935e5e2f4dadeb533aa
omarqc <>
2023-05-17 13:31:50 +0200

    code update

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import pyvisa
 import keyboard
 import numpy as np
 from random import randint
+from threading import Thread
 
 from scipy.optimize import curve_fit
 from pyqtgraph.Qt import QtGui, QtCore
@@ -15,72 +16,16 @@ import pyqtgraph as pg
 MAX_VIEW = 350
 PAUSE_FLAG = True
 
-description = {
-    "gaussmeter": {
-        "name": "MAGSYS HGM09s",
-        "idVendor": 0x04d8,
-        "idProduct": 0xfe78
-    }
-}
 
-FIND_ALL = False
-DEVICES = {}
+times1x, times1y, times1z, times2, times3 = [], [], [], [], []
+B_mag, B_x, B_y, B_z, resistance, temperature = [], [], [], [], [], []
 
-def detach_kernel_driver(dev, name):
-    try:
-        if DEVICES[name].is_kernel_driver_active(0):
-            DEVICES[name].detach_kernel_driver(0)
-                
-        print(f"Succesfully connected to '{name}'")
 
-    except:
-        print(f"Error in connecting to '{name}'")
-
-# Attempt connection to MAGSYS HGM09s gaussmeters
-for d_type in list(description.keys()):
-    IDV = description[d_type]["idVendor"]
-    IDP = description[d_type]["idProduct"]
-
-    devices = usb.core.find(idVendor=IDV, idProduct=IDP, find_all=FIND_ALL)
-    
-    try: 
-        # will give error if device variable is None (not found)
-        device[0]
-        try:
-            devices = tuple(devices)
-        except:
-            pass
-
-    except:
-        print(f"Device {d_type} not found, exiting")
-        break
-
-    # this is for case of >1 gaussmeter
-    i = 0
-    for sub_device in devices:
-        name = d_type + str(i)
-        DEVICES[name] = sub_device
-        detach_kernel_driver(DEVICES[name], description[d_type]['name']+str(i))
-
-        i += 1
-
-# Attempt connection to Keithley 2000 Multimeter
-rm = pyvisa.ResourceManager('@py')
-print(rm.list_resources())
-try:
-    keithley = rm.open_resource("ASRL/dev/ttyUSB0::INSTR", timeout=5000)
-    keithley.baud_rate = 19200
-    keithley.data_bits = 8
-    keithley.parity = pyvisa.constants.Parity.none
-    keithley.stop_bits = pyvisa.constants.StopBits.one
-
-    print(f"Succesfully connected to Keithley 2000 Multimeter")
-except Exception as e:
-    keithley=None
-    print(e)
-    print(f"Error in connecting to Keithley 2000 Multimeter")
-
-DEVICES["ohmmeter"] = keithley
+def reset_data():
+    global times1x, times1y, times1z, times2, times3
+    global B_mag, B_x, B_y, B_z, resistance, temperature
+    times1x, times1y, times1z, times2, times3 = [], [], [], [], []
+    B_mag, B_x, B_y, B_z, resistance, temperature = [], [], [], [], [], []
 
 
 def get_magnetic_field(device):
@@ -99,18 +44,75 @@ def get_resistance(device):
     return time.time(), randint(0,100)
 
 
+description = {
+    "gaussmeter": {
+        "name": "MAGSYS HGM09s",
+        "idVendor": 0x04d8,
+        "idProduct": 0xfe78
+    }
+}
+
+FIND_ALL = True
+DEVICES = {}
+
+
+def detach_kernel_driver(dev, name):
+    try:
+        if dev.is_kernel_driver_active(0):
+            dev.detach_kernel_driver(0)
+                
+        print(f"Succesfully connected to '{name}'")
+
+    except Exception as e:
+        print(f"Error in connecting to '{name}'")
+
+
+# Attempt connection to MAGSYS HGM09s gaussmeters
+for d_type in list(description.keys()):
+    IDV = description[d_type]["idVendor"]
+    IDP = description[d_type]["idProduct"]
+
+    devices = tuple(usb.core.find(idVendor=IDV, idProduct=IDP, find_all=FIND_ALL))
+    print(devices)
+    try: 
+        # will give error if device variable is None (not found)
+        devices[0]
+    except:
+        print(f"Device {d_type} not found, exiting")
+        break
+
+    # this is for case of >1 gaussmeter
+    i = 0
+    for sub_device in devices:
+        name = d_type + str(i)
+        DEVICES[name] = sub_device
+        detach_kernel_driver(DEVICES[name], description[d_type]['name']+str(i))
+
+        i += 1
+
+
+def connect_keithley():
+    # Attempt connection to Keithley 2000 Multimeter
+    rm = pyvisa.ResourceManager('@py')
+    print(rm.list_resources())
+    try:
+        keithley = rm.open_resource("ASRL/dev/ttyUSB0::INSTR", timeout=5000)
+        keithley.baud_rate = 19200
+        keithley.data_bits = 8
+        keithley.parity = pyvisa.constants.Parity.none
+        keithley.stop_bits = pyvisa.constants.StopBits.one
+
+        print(f"Succesfully connected to Keithley 2000 Multimeter")
+        
+    except Exception as e:
+        keithley=None
+        print(e)
+        print(f"Error in connecting to Keithley 2000 Multimeter")
+
+    DEVICES["ohmmeter"] = keithley
+
+
 app = QtGui.QApplication([])
-
-times1x, times1y, times1z, times2, times3 = [], [], [], [], []
-B_mag, B_x, B_y, B_z, resistance, temperature = [], [], [], [], [], []
-
-
-def reset_data():
-    global times1x, times1y, times1z, times2, times3
-    global B_mag, B_x, B_y, B_z, resistance, temperature
-    times1x, times1y, times1z, times2, times3 = [], [], [], [], []
-    B_mag, B_x, B_y, B_z, resistance, temperature = [], [], [], [], [], []
-
 
 window = pg.GraphicsWindow(title="Physics Lab 2 Data.")
 window.setBackground((238,238,228))
@@ -127,12 +129,13 @@ def make_plot(name, units, color):
 
     return curve, plot
 
+
 curve1, magnetic_field_plot = make_plot("Magnetic Field", "T", "green")
 curve2, resistance_plot = make_plot("Resistance", "&Omega;", "brown")
 curve3, temperature_plot = make_plot("Temperature", "K", "blue")
 
 
-def update(N=1, x:list=[], y:list=[], devs:list=[], func="", curve="", plot="", mag:list=[], ON=True):
+def update(N=1, x=[], y=[], devs=[], func="", curve="", plot="", mag=[], ON=True):
     # Get 3 measurements from the 3 gaussmeters
     # timestamp, value = get_magnetic_field(device)
 
@@ -157,7 +160,7 @@ def update(N=1, x:list=[], y:list=[], devs:list=[], func="", curve="", plot="", 
         X = x[0][-MAX_VIEW:]
         curve.setData(x[0], y[0])
     else:
-        # for case of magnetometer, plot time of middle reading y. (x, y, z)
+        # for case of magnetometer, plot time of middle reading (y)
         X = x[1][-MAX_VIEW:]
         curve.setData(x[1], mag)
 
@@ -180,20 +183,19 @@ while True:
     else:
         try:
             # graph update function for Gaussmeters
-            update(N=3, x=[times1x, times1y, times1z], y=[B_x, B_y, B_z], devs=[], func=get_magnetic_field, 
-                curve=curve1, plot=magnetic_field_plot, mag=B_mag, ON=False)
+            update(N=3, x=[times1x, times1y, times1z], y=[B_x, B_y, B_z], devs=[DEVICES["gaussmeter0"],DEVICES["gaussmeter1"],DEVICES["gaussmeter2"]], func=get_magnetic_field, 
+                curve=curve1, plot=magnetic_field_plot, mag=B_mag, ON=True)
 
             # graph update function for Ohmmeter
-            update(N=1, x=[times2], y=[resistance], devs=[], func=get_resistance, curve=curve2, plot=resistance_plot, ON=False)
+            # update(N=1, x=[times2], y=[resistance], devs=[], func=get_resistance, curve=curve2, plot=resistance_plot, ON=False)
 
-            # graph update function for Temperature Sensor
-            update(N=1, x=[times3], y=[temperature], devs=[], func=get_temperature, curve=curve3, plot=temperature_plot, ON=False)
+            # # graph update function for Temperature Sensor
+            # update(N=1, x=[times3], y=[temperature], devs=[], func=get_temperature, curve=curve3, plot=temperature_plot, ON=False)
 
         except KeyboardInterrupt:
             break
 
-
-print("Logging data to files (DO NOT CANCEL)...", end="")
+print("\nLogging data to files (DO NOT CANCEL)...", end="")
 # Save data to files
 b_file = open("magnetic_field.txt", "a")
 b_file.write("TimeX,TimeY,TimeZ,Bx,By,Bz")
