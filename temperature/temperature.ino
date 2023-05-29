d6b7bf84cc90040dabe59bb6fc8ac7e43390aa33
omarqc <>
2023-05-29 20:02:16 +0200

    readded old code, two options available

--- temperature/temperature.ino
+++ temperature/temperature.ino
@@ -1,20 +1,37 @@
 #define R_REF 1000.0
 #define AVERAGES 10
+#define DELAY 20
+
+int ready = 0;
 
 void setup() {
   Serial.begin(9600);
 }
 
+/*
+OPTION 1: print current resistance value upon request of 'main.py'
+*/
 void loop()
 {
-  while(Serial.available)
+  if (ready == 1)
   {
-    if (Serial.read() == 'r')
+    while(Serial.available)
     {
-      Serial.println(get_resistance());
+      if (Serial.read() == 'r')
+      {
+        Serial.println(millis() + ',' + String(get_resistance()));
+      }
+      delay(5);
+    }
+
+  } else 
+  {
+    // start non-stop data collection upon initial 'main.py' request
+    if (Serial.read() == 's') 
+    {
+      ready = 1;
     }
   }
-  delay(5);
 }
 
 float get_resistance()
@@ -24,3 +41,30 @@ float get_resistance()
   r = R_REF * adc/(1024.0 - adc);
   return r;
 }
+
+/* 
+OPTION 2: constantly print resistance data.
+'main.py' will read the data more slowly. Real-time plot is delayed depending of loop() frequency
+*/
+
+/*
+void loop()
+{
+  if (ready == 1)
+  {
+    float r;
+    adc = analogRead(A0);
+    r = R_REF * adc/(1024.0 - adc);
+    Serial.println(millis() + ',' + String(r));
+    delay(DELAY);
+  } else 
+  {
+    // start non-stop data collection upon initial 'main.py' request
+    if (Serial.read() == 's') 
+    {
+      ready = 1;
+    }
+  }
+}
+
+*/
