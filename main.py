562304604f644372275bfd13d34888f46aa09cc8
omarqc <>
2023-05-30 14:28:13 +0200

    fixed temperature timestamp

--- main.py
+++ main.py
@@ -34,13 +34,15 @@ Example: DEVICES["gaussmeter0"] or DEVICES["thermometer"]
 """
 DEVICES = {}
 
-ARDUINO_DATA_ON_TRIGGER = True
+ARDUINO_DATA_ON_TRIGGER = False
 ARDUINO_PORT = "/dev/ttyACM0"
 T_R0 = 100
 T_A = 3.9083e-3
 T_B = -5.775e-7
 T_C = -4.183e-12
 
+global T_START
+T_START = 0
 
 times1x, times1y, times1z, times2, times3 = [], [], [], [], []
 B_mag, B_x, B_y, B_z, resistance, temperature = [], [], [], [], [], []
@@ -66,8 +68,9 @@ def get_temperature(device):
         device.write(bytes('r', 'utf-8'))
     
     data = device.readline()[:-2].decode('utf-8').split(',')
-    t = float(data[0]) # arduino time very closely equal to time.time()-REF_TIME
-    data = float(data[1])/1000 # ms to s
+    print(data)
+    t = float(data[0])/1000 # arduino time very closely equal to time.time()-REF_TIME
+    data = float(data[1]) # ms to s
     # print(data)
     if data < T_R0:
         data = np.roots([T_R0*T_C, -100*T_C*T_R0, T_R0*T_B, T_R0*T_A,T_R0-float(data)])[-1].real + 273.15 # to kelvin
@@ -78,8 +81,7 @@ def get_temperature(device):
 
 
 def begin_thermistor():
-    DEVICE["thermometer"].write(bytes('s', 'utf-8'))
-    return "Starting Arduino..."
+    DEVICES["thermometer"].write(bytes('s', 'utf-8'))
 
 
 def get_resistance(device):
@@ -194,8 +196,10 @@ curve1, magnetic_field_plot = make_plot("Magnetic Field", "T", "green")
 curve2, resistance_plot = make_plot("Resistance", "&Omega;", "brown")
 curve3, temperature_plot = make_plot("Temperature", "K", "blue")
 
-
+global u
+u = False
 def update(N=1, x=[], y=[], func="", devs=[], curve="", plot="", mag=[], ON=True):
+    global u, T_START
     """
     Arguments:
         N: number of devices per measurement type
@@ -211,7 +215,6 @@ def update(N=1, x=[], y=[], func="", devs=[], curve="", plot="", mag=[], ON=True
     Returns:
         No return value
     """
-
     values = []
     for i in range(N):
         if ON:
@@ -221,7 +224,15 @@ def update(N=1, x=[], y=[], func="", devs=[], curve="", plot="", mag=[], ON=True
             t, v = time.time(), randint(0, 100)
 
         # append x values to the lists given as parameters
-        x[i].append(t - REF_TIME)
+        if func==get_temperature:
+            if not u:
+                x[i].append(0)
+                T_START = t
+                u = True
+            else:
+                x[i].append(t-T_START)
+        else:
+            x[i].append(t-REF_TIME)
         # append y values to the lists given as parameters
         y[i].append(v)
         # append y values to another list for internal reference
@@ -241,75 +252,92 @@ def update(N=1, x=[], y=[], func="", devs=[], curve="", plot="", mag=[], ON=True
  
     QtGui.QGuiApplication.processEvents() 
 
-
+print("Starting Arduino... ", end="")
+time.sleep(5)
+print("done")
+begin_thermistor() # Start running Arduino thermistor code
 REF_TIME = time.time()
-print(begin_thermistor()) # Start running Arduino thermistor code
 
 while True:
-    try:
-        if keyboard.is_pressed("space"):
-            PAUSE_FLAG = not PAUSE_FLAG
+    if keyboard.is_pressed("space"):
+        PAUSE_FLAG = not PAUSE_FLAG
 
-        if keyboard.is_pressed("r"):
-            # Reset ALL the data (x and y) arrays
-            REF_TIME = time.time()
-            reset_data()
-        
-        else:
-                # graph update function for Gaussmeters
-                update(N=3, x=[times1x, times1y, times1z], y=[B_x, B_y, B_z], devs=[DEVICES["gaussmeter0"], DEVICES["gaussmeter1"],
-                    DEVICES["gaussmeter2"]], func=get_magnetic_field, curve=curve1, plot=magnetic_field_plot, mag=B_mag, ON=True)
+    if keyboard.is_pressed("r"):
+        # Reset ALL the data (x and y) arrays
+        REF_TIME = time.time()
+        reset_data()
+            
+    else:
+        try:
+            # graph update function for Gaussmeters
+            # update(N=3, x=[times1x, times1y, times1z], y=[B_x, B_y, B_z], devs=[DEVICES["gaussmeter0"], DEVICES["gaussmeter1"],
+                    #DEVICES["gaussmeter2"]], func=get_magnetic_field, curve=curve1, plot=magnetic_field_plot, mag=B_mag, ON=True)
 
 
-                # uncomment for using only 1 gaussmeter (and comment the one above)
-                # update(N=1, x=[times1x], y=[B_x], devs=[DEVICES["gaussmeter0"], func=get_magnetic_field, curve=curve1,
-                #     plot=magnetic_field_plot, mag=B_mag, ON=True)
+            # uncomment for using only 1 gaussmeter (and comment the one above)
+            # update(N=1, x=[times1x], y=[B_x], devs=[], func=get_magnetic_field, curve=curve1,
+                    #plot=magnetic_field_plot, mag=B_mag, ON=False)
 
-                # graph update function for Ohmmeter
-                update(N=1, x=[times2], y=[resistance], devs=[DEVICES["ohmmeter"]], func=get_resistance, curve=curve2,
+            # graph update function for Ohmmeter
+            update(N=1, x=[times2], y=[resistance], devs=[DEVICES["ohmmeter"]], func=get_resistance, curve=curve2,
                     plot=resistance_plot, ON=True)
 
-                # # graph update function for Temperature Sensor
-                update(N=1, x=[times3], y=[temperature], devs=[DEVICES["thermometer"]], func=get_temperature, curve=curve3,
+            # graph update function for Temperature Sensor
+            update(N=1, x=[times3], y=[temperature], devs=[DEVICES["thermometer"]], func=get_temperature, curve=curve3,
                     plot=temperature_plot, ON=True)
 
-    except Exception as e:
-            # If there is any error, break off loop without cancelling program
-            # and proceed to saving data to files
-        print(e)
-        break
+        except KeyboardInterrupt as e:
+                # If there is any error, break off loop without cancelling program
+                # and proceed to saving data to files
+            print(e)
+            break
+        
+        except Exception as e:
+            print(e)
+            break
 
 print("\nLogging data to files (DO NOT CANCEL)...", end="")
 
 data_id = datetime.datetime.today().strftime("__%d_%m-%H_%M_%S")
 
 # Save data to files
-b_file = open(f"data/magnetic_field{data_id}.txt", "a")
-b_file.write("TimeX,TimeY,TimeZ,Bx,By,Bz")
-for i in range(len(times1x)):
-    b_file.write(f"\n{times1x[i]},{times1y[i]},{times1z[i]},{B_x[i]},{B_y[i]},{B_z[i]}")
+try:
+    b_file = open(f"data/magnetic_field{data_id}.txt", "a")
+    b_file.write("TimeX,TimeY,TimeZ,Bx,By,Bz")
+    for i in range(len(times1x)):
+        b_file.write(f"\n{times1x[i]},{times1y[i]},{times1z[i]},{B_x[i]},{B_y[i]},{B_z[i]}")
 
-# b_file.write("TimeX,Bx")
-# for i in range(len(times1x)):
-#     b_file.write(f"\n{times1x[i]},{B_x[i]}")
+    # b_file.write("TimeX,Bx")
+    # for i in range(len(times1x)):
+    #     b_file.write(f"\n{times1x[i]},{B_x[i]}")
 
-b_file.close()
+    b_file.close()
+
+except:
+    print("\nError logging magnetic field.")
+
+try:
+    r_file = open(f"data/resistance.txt{data_id}", "a")
+    r_file.write("Time,Resistance")
+    for i in range(len(times2)):
+        r_file.write(f"\n{times2[i]},{resistance[i]}")
+
+    r_file.close()
+
+except:
+    print("\nError logging resistance.")
 
 
-r_file = open(f"data/resistance.txt{data_id}", "a")
-r_file.write("Time,Resistance")
-for i in range(len(times2)):
-    r_file.write(f"\n{times2[i]},{resistance[i]}")
+try:
+    t_file = open(f"data/temperature.txt{data_id}", "a")
+    t_file.write("Time,Resistance,Temperature")
+    for i in range(len(times3)):
+        T = temperature[i]
+        t_file.write(f"\n{times3[i]},{T}")
 
-r_file.close()
+    t_file.close()
 
+except:
+    print("\nError logging temperature.")
 
-t_file = open(f"data/temperature.txt{data_id}", "a")
-t_file.write("Time,Resistance,Temperature")
-for i in range(len(times3)):
-    T = temperature[i]
-    t_file.write(f"\n{times3[i]},{T}")
-
-t_file.close()
-
-print(" DONE")
+print(" DONE!")
